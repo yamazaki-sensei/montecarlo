29efa1009dbeae70a494ec30509a899c7a35ad6e
hira <ohno@toreta.in>
2020-01-03 14:55:09 +0900

    Add project files

---
+++ montecarlo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  montecarlo
+//
+//  Created by Hiraku Ohno on 2020/01/03.
+//  Copyright © 2020 hira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
